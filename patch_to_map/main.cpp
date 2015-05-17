fd54c7758db539129454918fe74be73d4f6c01aa
Brian Miles <selimnairb@gmail.com>
2015-05-17 13:18:03 -0400

    Initial Commit

---
+++ patch_to_map/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  patch_to_map
+//
+//  Created by Brian C. Miles on 5/17/15.
+//  Copyright (c) 2015 selimnairb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
